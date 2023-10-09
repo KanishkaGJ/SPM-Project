946cdf651b009db9461c046084d9d38c31cfc65f
MeharaSahabandu <sahabandumehara@gmail.com>
2023-10-10 01:47:25 +0530

    Navbar added to profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import { auth, fs } from "../Config/Config";
 import { useHistory } from "react-router-dom";
 import Swal from "sweetalert2";
-
+import { Navbar } from "./Navbar";
+import { arrowLeft } from "react-icons-kit/feather/arrowLeft";
+import { Icon } from "react-icons-kit";
 export const Profile = () => {
   const [userProfile, setUserProfile] = useState(null);
   const [editing, setEditing] = useState(false);
   const [editedProfile, setEditedProfile] = useState({});
   const history = useHistory();
-
   useEffect(() => {
     const currentUser = auth.currentUser;
     if (currentUser) {
@@ -28,21 +29,17 @@ export const Profile = () => {
         });
     }
   }, []);
-
   const enableEditing = () => {
     setEditing(true);
   };
-
   const handleEditChange = (e) => {
     const { name, value } = e.target;
     setEditedProfile({ ...editedProfile, [name]: value });
   };
-
   const saveEditedProfile = () => {
-    // Check if there are any changes in the edited profile
     const isProfileChanged =
       JSON.stringify(editedProfile) !== JSON.stringify(userProfile);
-  
+
     if (isProfileChanged) {
       Swal.fire({
         title: "Are you sure?",
@@ -60,16 +57,23 @@ export const Profile = () => {
             .then(() => {
               setUserProfile(editedProfile);
               setEditing(false);
-              Swal.fire("Success!", "Your profile has been updated.", "success");
+              Swal.fire(
+                "Success!",
+                "Your profile has been updated.",
+                "success"
+              );
             })
             .catch((error) => {
               console.error("Error updating document:", error);
-              Swal.fire("Error", "An error occurred while updating your profile.", "error");
+              Swal.fire(
+                "Error",
+                "An error occurred while updating your profile.",
+                "error"
+              );
             });
         }
       });
     } else {
-      // Show a message if no changes were made
       Swal.fire({
         title: "No Changes",
         text: "You haven't made any changes to your profile. Do you want to proceed anyway?",
@@ -85,7 +89,17 @@ export const Profile = () => {
       });
     }
   };
-  
+  const [totalProducts, setTotalProducts] = useState(0);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        fs.collection("Cart " + user.uid).onSnapshot((snapshot) => {
+          const qty = snapshot.docs.length;
+          setTotalProducts(qty);
+        });
+      }
+    });
+  }, []);
   const deleteAccount = () => {
     Swal.fire({
       title: "Are you sure?",
@@ -108,170 +122,198 @@ export const Profile = () => {
       }
     });
   };
-
+  function GetCurrentUser() {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+      auth.onAuthStateChanged((user) => {
+        if (user) {
+          fs.collection("users")
+            .doc(user.uid)
+            .get()
+            .then((snapshot) => {
+              setUser(snapshot.data().FullName);
+            });
+        } else {
+          setUser(null);
+        }
+      });
+    }, []);
+    return user;
+  }
+  const user = GetCurrentUser();
   return (
-    <div className="container">
-      <br />
-      <br />
-      <center>
-        <h1>User Profile</h1>
-      </center>
-      <hr />
-      {userProfile && (
-        <div>
-          <div className="profile-header">
-            <center>
-              {" "}
-              <div
-                className="profile-avatar"
-                style={{
-                  width: "100px",
-                  height: "100px",
-                  backgroundColor: "black",
-                  borderRadius: "50%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  marginTop: "2%",
-                }}
-              >
+    <div>
+      <Navbar user={user} totalProducts={totalProducts} />
+      <div className="container">
+        <br />
+        <br />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <button
+            className="btn btn-link"
+            onClick={() => history.goBack()} // Go back to the previous page
+          >
+            <Icon icon={arrowLeft} size={34} style={{ marginTop:"2%", marginLeft:"-100%", color:"#000000" }} /> {/* Render the back button icon */}
+          </button>
+          
+        </div>
+        <center><h1 style={{ marginTop:"-3%" }} >User Profile</h1></center>
+      
+        <hr />
+        {userProfile && (
+          <div>
+            <div className="profile-header">
+              <center>
+                {" "}
                 <div
-                  className="avatar-letter"
-                  style={{ color: "white", fontSize: "44px" }}
+                  className="profile-avatar"
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    backgroundColor: "black",
+                    borderRadius: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginTop: "2%",
+                  }}
                 >
-                  {userProfile.FullName.charAt(0).toUpperCase()}
+                  <div
+                    className="avatar-letter"
+                    style={{ color: "white", fontSize: "44px" }}
+                  >
+                    {userProfile.FullName.charAt(0).toUpperCase()}
+                  </div>
                 </div>
-              </div>
-            </center>
-
-            <div className="profile-details">
-              {editing ? (
-                <form>
+              </center>
+              <div className="profile-details">
+                {editing ? (
+                  <form>
+                    <table
+                      className="table table-borderless"
+                      style={{ marginTop: "3%", marginLeft: "32%" }}
+                    >
+                      <tbody>
+                        <tr>
+                          <th style={{ width: "25%" }}>Full Name:</th>
+                          <td>
+                            <input
+                              type="text"
+                              style={{ width: "25%" }}
+                              className="form-control"
+                              id="FullName"
+                              name="FullName"
+                              value={editedProfile.FullName}
+                              onChange={handleEditChange}
+                            />
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Email:</th>
+                          <td>
+                            <input
+                              type="email"
+                              style={{ width: "25%" }}
+                              className="form-control"
+                              id="Email"
+                              name="Email"
+                              value={editedProfile.Email}
+                              onChange={handleEditChange}
+                            />
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Contact Number:</th>
+                          <td>
+                            <input
+                              type="text"
+                              style={{ width: "25%" }}
+                              className="form-control"
+                              id="ContactNumber"
+                              name="ContactNumber"
+                              value={editedProfile.ContactNumber}
+                              onChange={handleEditChange}
+                            />
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Address:</th>
+                          <td>
+                            <input
+                              type="text"
+                              style={{ width: "25%" }}
+                              className="form-control"
+                              id="Address"
+                              name="Address"
+                              value={editedProfile.Address}
+                              onChange={handleEditChange}
+                            />
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </form>
+                ) : (
                   <table
                     className="table table-borderless"
                     style={{ marginTop: "3%", marginLeft: "32%" }}
                   >
                     <tbody>
                       <tr>
-                        <th style={{ width: "25%" }}>Full Name:</th>
-                        <td>
-                          <input
-                            type="text"
-                            style={{ width: "25%" }}
-                            className="form-control"
-                            id="FullName"
-                            name="FullName"
-                            value={editedProfile.FullName}
-                            onChange={handleEditChange}
-                          />
-                        </td>
+                        <th style={{ width: "25%" }}>Full Name</th>
+                        <td>{userProfile.FullName}</td>
                       </tr>
+                      <br />
                       <tr>
-                        <th>Email:</th>
-                        <td>
-                          <input
-                            type="email"
-                            style={{ width: "25%" }}
-                            className="form-control"
-                            id="Email"
-                            name="Email"
-                            value={editedProfile.Email}
-                            onChange={handleEditChange}
-                          />
-                        </td>
+                        <th>Email</th>
+                        <td>{userProfile.Email}</td>
                       </tr>
+                      <br />
                       <tr>
-                        <th>Contact Number:</th>
-                        <td>
-                          <input
-                            type="text"
-                            style={{ width: "25%" }}
-                            className="form-control"
-                            id="ContactNumber"
-                            name="ContactNumber"
-                            value={editedProfile.ContactNumber}
-                            onChange={handleEditChange}
-                          />
-                        </td>
+                        <th>Contact Number</th>
+                        <td>{userProfile.ContactNumber}</td>
                       </tr>
+                      <br />
                       <tr>
-                        <th>Address:</th>
-                        <td>
-                          <input
-                            type="text"
-                            style={{ width: "25%" }}
-                            className="form-control"
-                            id="Address"
-                            name="Address"
-                            value={editedProfile.Address}
-                            onChange={handleEditChange}
-                          />
-                        </td>
+                        <th>Address</th>
+                        <td>{userProfile.Address}</td>
                       </tr>
                     </tbody>
                   </table>
-                </form>
-              ) : (
-                <table
-                  className="table table-borderless"
-                  style={{ marginTop: "3%", marginLeft: "32%" }}
-                >
-                  <tbody>
-                    <tr>
-                      <th style={{ width: "25%" }}>Full Name</th>
-                      <td>{userProfile.FullName}</td>
-                    </tr>
-                    <br />
-                    <tr>
-                      <th>Email</th>
-                      <td>{userProfile.Email}</td>
-                    </tr>
-                    <br />
-                    <tr>
-                      <th>Contact Number</th>
-                      <td>{userProfile.ContactNumber}</td>
-                    </tr>
-                    <br />
-                    <tr>
-                      <th>Address</th>
-                      <td>{userProfile.Address}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              )}
+                )}
+              </div>
             </div>
-          </div>
-          <center>
-            <div style={{ marginTop: "30px" }}>
-              {editing ? (
+            <center>
+              <div style={{ marginTop: "30px" }}>
+                {editing ? (
+                  <button
+                    className="btn btn-success"
+                    onClick={saveEditedProfile}
+                    style={{ marginLeft: "20px" }}
+                  >
+                    Save
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-primary"
+                    onClick={enableEditing}
+                    style={{ marginLeft: "25px" }}
+                  >
+                    Edit Profile
+                  </button>
+                )}
                 <button
-                  className="btn btn-success"
-                  onClick={saveEditedProfile}
+                  className="btn btn-danger"
+                  onClick={deleteAccount}
                   style={{ marginLeft: "20px" }}
                 >
-                  Save
+                  Delete Account
                 </button>
-              ) : (
-                <button
-                  className="btn btn-primary"
-                  onClick={enableEditing}
-                  style={{ marginLeft: "25px" }}
-                >
-                  Edit Profile
-                </button>
-              )}
-              <button
-                className="btn btn-danger"
-                onClick={deleteAccount}
-                style={{ marginLeft: "20px" }}
-              >
-                Delete Account
-              </button>
-            </div>
-          </center>
-        </div>
-      )}
-      {!userProfile && <p>Loading...</p>}
+              </div>
+            </center>
+          </div>
+        )}
+        {!userProfile && <p>Loading...</p>}
+      </div>
     </div>
   );
 };
