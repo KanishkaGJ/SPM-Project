9dc78decb0331407bdfde1c2e976f3364f6ff5be
Sandali Disara <sandalidissanayake123@gmail.com>
2023-10-09 23:14:06 +0530

    update SellerShop

--- src/Components/SellerShop.js
+++ src/Components/SellerShop.js
@@ -1,41 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fs } from '../Config/Config';
 
 export const SellerShop = () => {
   const [products, setProducts] = useState([]);
 
-  // Fetch the products from Firestore
-  const fetchProducts = async () => {
-    try {
-      const productsRef = fs.collection('Products'); // Assuming 'Products' is your collection name
-      const snapshot = await productsRef.get();
-      const productsData = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setProducts(productsData);
-    } catch (error) {
-      console.error('Error fetching products: ', error);
-    }
-  };
-
   useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productsRef = await fs.collection('Products').get();
+        const productsArray = productsRef.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setProducts(productsArray);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
     fetchProducts();
   }, []);
 
   return (
-    <div className="seller-shop">
-      <h1>Seller's Shop</h1>
-      <div className="products-list">
+    <div className="container">
+      <h1 className="text-center">Seller's Shop</h1>
+      <div className="row">
         {products.map((product) => (
-          <div key={product.id} className="product-card">
-            <img src={product.imageURL} alt={product.name} />
-            <h2>{product.name}</h2>
-            <p>Price: ${product.price}</p>
+          <div key={product.id} className="col-lg-4 col-md-6 col-sm-12">
+            <div className="card mb-4">
+              <img src={product.imageURL} className="card-img-top" alt={product.title} />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">${product.price}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
     </div>
   );
 };
+
 
