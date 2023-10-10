05ddc757108a21a3091467105b112307dccf8f65
MeharaSahabandu <sahabandumehara@gmail.com>
2023-10-10 16:39:13 +0530

    Login ans signup screens changed

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,63 +1,102 @@
-import React,{useState} from 'react'
-import {Link} from 'react-router-dom'
-import {auth} from '../Config/Config'
-import {useHistory} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { auth } from "../Config/Config";
+import { useHistory } from "react-router-dom";
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 export const Login = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
 
-    const history = useHistory();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        setSuccessMsg(
+          "Login Successfull. You will now automatically get redirected to Home page"
+        );
+        setEmail("");
+        setPassword("");
+        setErrorMsg("");
+        setTimeout(() => {
+          setSuccessMsg("");
+          history.push("/");
+        }, 3000);
+      })
+      .catch((error) => setErrorMsg(error.message));
+  };
+  const handleSignupClick = () => {
+    history.push("/signup");
+  };
 
-    const [email, setEmail]=useState('');
-    const [password, setPassword]=useState('');
+  const loginButtonStyle = {
+    marginTop: "1%",
+    backgroundColor: "black",
+    color: "white", 
+  };
 
-    const [errorMsg, setErrorMsg]=useState('');
-    const [successMsg, setSuccessMsg]=useState('');
-
-    const handleLogin=(e)=>{
-        e.preventDefault();
-        // console.log(email, password);
-        auth.signInWithEmailAndPassword(email,password).then(()=>{
-            setSuccessMsg('Login Successfull. You will now automatically get redirected to Home page');
-            setEmail('');
-            setPassword('');
-            setErrorMsg('');
-            setTimeout(()=>{
-                setSuccessMsg('');
-                history.push('/');
-            },3000)
-        }).catch(error=>setErrorMsg(error.message));
-    }
-
-    return (
-        <div className='container'>
+  return (
+    <div className="container" style={{ marginTop: "10%" }}>
+      <center>
+        <br></br>
+        <br></br>
+        <h1>Login</h1>
+        {successMsg && (
+          <>
+            <div className="success-msg">{successMsg}</div>
             <br></br>
+          </>
+        )}
+        <form className="form-group" autoComplete="off" onSubmit={handleLogin}>
+          <TextField
+            id="standard-basic"
+            label="Email"
+            variant="standard"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <br />
+          <br />
+          <TextField
+            id="standard-basic"
+            label="Password"
+            variant="standard"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <br />
+          <br />
+          <Button type="submit" variant="outlined" style={loginButtonStyle}>
+            LOGIN
+          </Button>
+        </form>
+        <br />
+        <br />
+        <p>Don't have an account ?</p>
+        <Box sx={{ "& button": { m: 1 } }}>
+          <Button
+            size="large"
+            style={{ marginTop: "-1%", color: "black" }}
+            onClick={handleSignupClick}
+          >
+            SIGNUP
+          </Button>
+        </Box>
+        {errorMsg && (
+          <>
             <br></br>
-            <h1>Login</h1>
-            <hr></hr>
-            {successMsg&&<>
-                <div className='success-msg'>{successMsg}</div>
-                <br></br>
-            </>}
-            <form className='form-group' autoComplete="off"
-            onSubmit={handleLogin}>               
-                <label>Email</label>
-                <input type="email" className='form-control' required
-                onChange={(e)=>setEmail(e.target.value)} value={email}></input>
-                <br></br>
-                <label>Password</label>
-                <input type="password" className='form-control' required
-                onChange={(e)=>setPassword(e.target.value)} value={password}></input>
-                <br></br>
-                <div className='btn-box'>
-                    <span>Don't have an account SignUp
-                    <Link to="signup" className='link'> Here</Link></span>
-                    <button type="submit" className='btn btn-success btn-md'>LOGIN</button>
-                </div>
-            </form>
-            {errorMsg&&<>
-                <br></br>
-                <div className='error-msg'>{errorMsg}</div>                
-            </>}
-        </div>
-    )
-}
+            <div className="error-msg">{errorMsg}</div>
+          </>
+        )}
+      </center>
+    </div>
+  );
+};
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,77 +1,157 @@
-import React,{useState} from 'react'
-import {auth,fs} from '../Config/Config'
-import {Link} from 'react-router-dom'
-import {useHistory} from 'react-router-dom'
+import React, { useState } from "react";
+import { auth, fs } from "../Config/Config";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 export const Signup = () => {
+  const history = useHistory();
+  const [fullName, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [address, setAddress] = useState("");
 
-    const history = useHistory();
+  const [errorMsg, setErrorMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
 
-    const [fullName, setFullname]=useState('');
-    const [email, setEmail]=useState('');
-    const [password, setPassword]=useState('');
-
-    const [errorMsg, setErrorMsg]=useState('');
-    const [successMsg, setSuccessMsg]=useState('');
-
-    const handleSignup=(e)=>{
-        e.preventDefault();
-        // console.log(fullName, email, password);
-        auth.createUserWithEmailAndPassword(email,password).then((credentials)=>{
-            console.log(credentials);
-            fs.collection('users').doc(credentials.user.uid).set({
-                FullName: fullName,
-                Email: email,
-                Password: password
-            }).then(()=>{
-                setSuccessMsg('Signup Successfull. You will now automatically get redirected to Login');
-                setFullname('');
-                setEmail('');
-                setPassword('');
-                setErrorMsg('');
-                setTimeout(()=>{
-                    setSuccessMsg('');
-                    history.push('/login');
-                },3000)
-            }).catch(error=>setErrorMsg(error.message));
-        }).catch((error)=>{
-            setErrorMsg(error.message)
-        })
+  const handleSignup = (e) => {
+    e.preventDefault();
+    if (!contactNumber) {
+      setErrorMsg("Please enter a Contact Number.");
+      return;
     }
+    if (!address) {
+      setErrorMsg("Please enter your address");
+      return;
+    }
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((credentials) => {
+        fs.collection("users")
+          .doc(credentials.user.uid)
+          .set({
+            FullName: fullName,
+            Email: email,
+            Password: password,
+            ContactNumber: contactNumber,
+            Address: address,
+          })
+          .then(() => {
+            setSuccessMsg(
+              "Signup Successful. You will now automatically get redirected to Login"
+            );
+            setFullname("");
+            setEmail("");
+            setPassword("");
+            setContactNumber("");
+            setAddress("");
+            setErrorMsg("");
+            setTimeout(() => {
+              setSuccessMsg("");
+              history.push("/login");
+            }, 3000);
+          })
+          .catch((error) => setErrorMsg(error.message));
+      })
+      .catch((error) => {
+        setErrorMsg(error.message);
+      });
+  };
 
-    return (
-        <div className='container'>
+  const handleLoginClick = () => {
+    history.push("/login");
+  };
+
+  const loginButtonStyle = {
+    marginTop: "2%",
+    backgroundColor: "black",
+    color: "white",
+  };
+
+  return (
+    <div className="container" style={{ marginTop: "10%" }}>
+      <center>
+        <br></br>
+        <br></br>
+        <h1>Sign Up</h1>
+        {successMsg && (
+          <>
+            <div className="success-msg">{successMsg}</div>
             <br></br>
+          </>
+        )}
+        <form className="form-group" autoComplete="off" onSubmit={handleSignup}>
+          <TextField
+            id="fullname"
+            label="Full Name"
+            variant="standard"
+            required
+            onChange={(e) => setFullname(e.target.value)}
+            value={fullName}
+          />
+          <br></br>
+          <TextField
+            id="email"
+            label="Email"
+            variant="standard"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <br></br>
+          <TextField
+            id="password"
+            label="Password"
+            variant="standard"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <br></br>
+          <TextField
+            id="contactnumber"
+            label="Contact NUmber"
+            variant="standard"
+            required
+            onChange={(e) => setContactNumber(e.target.value)}
+            value={contactNumber}
+          />
+          <br></br>
+          <TextField
+            id="address"
+            label="Address"
+            variant="standard"
+            required
+            onChange={(e) => setAddress(e.target.value)}
+            value={address}
+          />
+          <br></br>
+          <Button type="submit" variant="outlined" style={loginButtonStyle}>
+            SIGN UP
+          </Button>
+        </form>
+        <br />
+        <br />
+        <p>Don't have an account ?</p>
+        <Box sx={{ "& button": { m: 1 } }}>
+          <Button
+            size="large"
+            style={{ marginTop: "-1%", color: "black" }}
+            onClick={handleLoginClick}
+          >
+            LOGIN
+          </Button>
+        </Box>
+        {errorMsg && (
+          <>
             <br></br>
-            <h1>Sign Up</h1>
-            <hr></hr>
-            {successMsg&&<>
-                <div className='success-msg'>{successMsg}</div>
-                <br></br>
-            </>}
-            <form className='form-group' autoComplete="off" onSubmit={handleSignup}>
-                <label>Full Name</label>
-                <input type="text" className='form-control' required
-                onChange={(e)=>setFullname(e.target.value)} value={fullName}></input>
-                <br></br>
-                <label>Email</label>
-                <input type="email" className='form-control' required
-                 onChange={(e)=>setEmail(e.target.value)} value={email}></input>
-                <br></br>
-                <label>Password</label>
-                <input type="password" className='form-control' required
-                 onChange={(e)=>setPassword(e.target.value)} value={password}></input>
-                <br></br>
-                <div className='btn-box'>
-                    <span>Already have an account Login
-                    <Link to="login" className='link'> Here</Link></span>
-                    <button type="submit" className='btn btn-success btn-md'>SIGN UP</button>
-                </div>
-            </form>
-            {errorMsg&&<>
-                <br></br>
-                <div className='error-msg'>{errorMsg}</div>                
-            </>}
-        </div>
-    )
-}
+            <div className="error-msg">{errorMsg}</div>
+          </>
+        )}
+      </center>
+    </div>
+  );
+};
